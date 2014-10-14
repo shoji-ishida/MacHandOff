d28bbaf4be8921be8053f0920e2fc0eaf6370db8
Shoji Ishida <shoji.ishida@gmail.com>
2014-10-14 10:41:34 +0900

    Working sample
    Currently oneway from iOS8 to Yosemite GM is working.
    Yosemite -> iOS8 failed due to unknown UserActivityType which is set to
    nil when a Handoff advertisement has been received at iOS8.

--- MacHandOff/AppDelegate.m
+++ MacHandOff/AppDelegate.m
@@ -10,6 +10,8 @@
 
 @interface AppDelegate ()
 
+@property (weak) IBOutlet NSTextField *textField;
+@property (weak) IBOutlet NSButton *checkButton;
 @property (weak) IBOutlet NSWindow *window;
 @end
 
@@ -17,10 +19,36 @@
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+    NSUserActivity* myActivity = [[NSUserActivity alloc]
+                                  initWithActivityType:@"jp.neoscorp.handoff.browsing"];
+    myActivity.userInfo = @{ @"text" : self.textField.stringValue,
+                             @"on" : [NSNumber numberWithInteger : self.checkButton.state]};
+    myActivity.title = @"Browsing";
+    [myActivity becomeCurrent];
+    [myActivity setDelegate:self];
+    [myActivity setNeedsSave:YES];
+    
+    [self.window setUserActivity:myActivity];
+
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+    
+    
 }
 
+- (void)userActivityWillSave:(NSUserActivity *)userActivity {
+    NSLog(@"ViewController:userActivityWillSave");
+    NSDictionary *dictionary = @{ @"text" : self.textField.stringValue,
+                                  @"on" : [NSNumber numberWithInteger : self.checkButton.state]};
+    [userActivity addUserInfoEntriesFromDictionary:dictionary];
+}
+
+- (void)restoreUserActivityState:(NSUserActivity *)userActivity {
+    NSLog(@"ViewController:restoreUserActivityState");
+    NSDictionary *dictionary = userActivity.userInfo;
+    NSLog(@"text = %@", [dictionary objectForKey:@"text"]);
+    NSLog(@"on = %@", [dictionary objectForKey:@"on"]);
+}
 @end
